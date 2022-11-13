545ed62d1a4eb915f1dcb27463d19547ab32f86f
郭明月 <mingyue.guo@keliangtek.com>
2022-11-13 22:22:43 +0800

    vscode test java

---
+++ java/java.java
@@ -0,0 +1,10 @@
+
+import java.lang.System;
+
+public class java {
+
+  public static void main(String[] args){
+    System.out.println("Hello vscode this from Java!");
+  }
+
+}
--- python/hello.py
+++ python/hello.py
@@ -1,4 +1,4 @@
 import os
 
-print("adfd")
+print("vscode from python")
 
